503a5162c23735e3c105b6373ada238c6c80adba
iwillens <igor@42sp.org.br>
2022-03-05 14:49:40 -0300

    nest && angular

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,3 +1,11 @@
-docker rmi node-image
-docker build --build-arg http_proxy=$http_proxy -t node-image .
-docker run --rm --name my-node -v $PWD/volume:/home/node:rw -p3000:3000 node-image
+CONTAINER_NAME=my-node
+IMAGE_NAME=node-image
+ROOT_USER=""
+if [ "$1" == "root" ]; then
+	ROOT_USER="-uroot";
+fi
+
+docker rm -f $CONTAINER_NAME
+docker rmi -f $IMAGE_NAME
+docker build --build-arg http_proxy=$http_proxy -t $IMAGE_NAME .
+docker run -ti --name $CONTAINER_NAME $ROOT_USER -v $PWD/volume:/home/node/sandbox:rw -v $PWD/../srcs:/home/node/srcs:rw $IMAGE_NAME /bin/bash
