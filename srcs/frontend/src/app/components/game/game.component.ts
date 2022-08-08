f0c29a43e6137af5ecb3c827bc63552815091678
Nayran Ziebell <nayranziebell@icloud.com>
2022-08-08 15:47:47 -0300

    menu game and finishing game at score

--- srcs/frontend/src/app/components/game/game.component.ts
+++ srcs/frontend/src/app/components/game/game.component.ts
@@ -3,6 +3,7 @@ import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { BallComponent } from './ball/ball.component';
 
 export const PADDLE_SPEED = 8;
+export const MAX_SCORE = 10;
 
 @Component({
   selector: 'app-game',
@@ -16,6 +17,10 @@ export class GameComponent implements OnInit {
   player1Score = 0;
   player2Score = 0;
   paused: boolean = false;
+  play: boolean = false;
+  finished: boolean = false;
+  message!: string;
+  mode!: string;
 
   constructor() { }
 
@@ -24,13 +29,13 @@ export class GameComponent implements OnInit {
   @ViewChild('player2') player2Paddle!: PaddleComponent;
   @ViewChild('computer') computerPaddle!: PaddleComponent;
 
-
   @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
     switch (event.key) {
       case 'Escape':
       case 'p':
       case 'P':
-        this.pause();
+        if (this.play && !this.finished)
+          this.pause();
         break;
 
       case 'w':
@@ -48,11 +53,22 @@ export class GameComponent implements OnInit {
       case 'ArrowUp':
         if (!this.paused && this.player2Paddle && Number(this.player2Paddle.rect.y) > 155)
           this.player2Paddle.position -= PADDLE_SPEED;
+        if (!this.paused && this.computerPaddle && Number(this.player1Paddle.rect.y) > 155)
+          this.player1Paddle.position -= PADDLE_SPEED;
         break;
 
       case 'ArrowDown':
         if (!this.paused && this.player2Paddle && Number(this.player2Paddle.rect.y) < 600)
           this.player2Paddle.position += PADDLE_SPEED;
+        if (!this.paused && this.computerPaddle && Number(this.player1Paddle.rect.y) < 600)
+          this.player1Paddle.position += PADDLE_SPEED;
+        break;
+
+      case 'q':
+      case 'Q':
+        if (this.paused || this.finished) {
+          window.location.reload();
+        }
         break;
 
       default:
@@ -61,10 +77,23 @@ export class GameComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
   ngAfterViewInit(): void {
-    this.update(0);
+  }
+
+  start(mode: string) {
+    this.mode = mode;
+    this.play = true;
+    this.player1Score = 0;
+    this.player2Score = 0;
+    this.lastTime = 0;
+    this.currentAnimationFrameId = window.requestAnimationFrame(this.update.bind(this));
+    this.ball.reset();
+    this.paused = false;
+    this.finished = false;
+    this.update(1);
   }
 
   update(time: number) {
@@ -84,9 +113,10 @@ export class GameComponent implements OnInit {
         this.handleLose();
       }
     }
-
     this.lastTime = time;
     this.currentAnimationFrameId = window.requestAnimationFrame(this.update.bind(this));
+    if (this.player1Score == MAX_SCORE || this.player2Score == MAX_SCORE)
+      this.finish();
   }
 
   isLose() {
@@ -113,6 +143,23 @@ export class GameComponent implements OnInit {
     }
     if (!this.paused) {
       window.requestAnimationFrame(this.update.bind(this));
+    }
+  }
+
+  finish() {
+    this.finished = true;
+    window.cancelAnimationFrame(this.currentAnimationFrameId as number);
+    if (this.computerPaddle) {
+      if (this.player1Score == MAX_SCORE)
+        this.message = "You won!";
+      else
+        this.message = "Computer won!";
+    }
+    else {
+      if (this.player1Score == MAX_SCORE)
+        this.message = "Player1 won!";
+      else
+        this.message = "Player2 won!";
     }
   }
 }
