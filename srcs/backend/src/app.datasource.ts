5d738a7b407aca59176e34c6316b3bb3c1e8f2fc
Roman <roman.bitetti@tutanota.com>
2022-08-16 14:53:43 -0400

    update dependencies front

--- srcs/backend/src/app.datasource.ts
+++ srcs/backend/src/app.datasource.ts
@@ -1,26 +1,38 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { AuthEntity } from './auth/models/auth.entity';
 import { RoomEntity } from './chat/models/room.entity';
 import { UserEntity } from './users/users.entity';
 
-const dbConfig: TypeOrmModuleOptions = {
+// const dbConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: process.env.POSTGRES_HOST || 'postgres',
+//   port: +process.env.POSTGRES_PORT || 5432,
+//   username: process.env.POSTGRES_USER || 'postgres',
+//   password: process.env.POSTGRES_PASSWORD || 'postgress',
+//   database: process.env.POSTGRES_DB || 'postgres',
+//   entities: [UserEntity, RoomEntity, AuthEntity],
+//   synchronize: true,
+// };
+
+const dbConfig: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST || 'postgres',
-  port: process.env.POSTGRES_PORT || 5432,
+  port: +process.env.POSTGRES_PORT || 5432,
   username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgress',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
   database: process.env.POSTGRES_DB || 'postgres',
   entities: [UserEntity, RoomEntity, AuthEntity],
-  synchronize: true,
-};
+  synchronize: true, // TO REMOVE FOR PRODUCTION!!!!
+  dropSchema: true // TO REMOVE FOR PRODUCTION!!!!
+}
 
 export const dataSource = new DataSource(dbConfig);
 
 dataSource.initialize()
   .then(() => {
-      console.log("Database has been initialized!")
+    console.log("Database has been initialized!")
   })
   .catch((err) => {
-      console.error("Error initializing Database", err)
+    console.error("Error initializing Database", err)
   });
