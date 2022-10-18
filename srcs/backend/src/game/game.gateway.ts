b44fbbc82347565202d1580383d843ff9fc1f109
Nayran Ziebell <nayranziebell@icloud.com>
2022-10-18 19:53:57 -0300

    game socket guard

--- srcs/backend/src/game/game.gateway.ts
+++ srcs/backend/src/game/game.gateway.ts
@@ -1,5 +1,7 @@
+import { UseGuards } from '@nestjs/common';
 import { WebSocketGateway, SubscribeMessage, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { TfaGuard } from 'src/auth/tfa/tfa.guard';
 
 @WebSocketGateway({ cors: '*:*' })
 export class GameGateway {
@@ -22,15 +24,22 @@ export class GameGateway {
 
   @SubscribeMessage('joinGame')
   joinGame(client: Socket) {
+    if (!this.player1) {
+      this.player1 = client.id;
+      console.log('player1 connected')
+    }
+    else if (!this.player2) {
+      this.player2 = client.id;
+      console.log('player2 connected')
+    }
+    else {
+      console.log('player3?')
+    }
     this.server.emit("position", this.positionP1, this.positionP2);
   }
 
   @SubscribeMessage('move')
   move(client: Socket, command: string) {
-    if (!this.player1)
-      this.player1 = client.id;
-    else
-      this.player2 = client.id;
     let position = this.positionP2;
     if (client.id == this.player1) {
       position = this.positionP1;
@@ -45,6 +54,18 @@ export class GameGateway {
         position.y += 5;
         this.server.emit("position", this.positionP1, this.positionP2);
         break;
+    }
+  }
+
+  @UseGuards(TfaGuard)
+  handleDisconnect(client: Socket, ...args: any[]) {
+    if (client.id == this.player1) {
+      this.player1 = null;
+      console.log('player1 disconnected')
+    }
+    else if (client.id == this.player2) {
+      this.player2 = null;
+      console.log('player2 disconnected')
     }
   }
 }
