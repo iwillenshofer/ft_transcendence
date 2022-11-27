21408f37010d948cfb6830e57f8677bbdb880b22
iwillens <igor@42sp.org.br>
2022-11-27 01:28:16 -0300

    login page

--- srcs/frontend/src/app/components/login/loginpage/loginpage.component.ts
+++ srcs/frontend/src/app/components/login/loginpage/loginpage.component.ts
@@ -12,17 +12,20 @@ class Ball {
 	public color: string = '';
 	public vx: number = 0;
 	public vy: number = 0;
+	public removing: boolean = false;
+	public alpha: number = 500;
 
-
-	constructor (canvas_x: number, canvas_y: number) {
-		let colors = ["#111111","#333333","#555555","#7777777","#999999","#bbbbbb","#dddddd","#ffffff", "#f47b37", "#fcb338", "#fcc816"];
+	constructor (canvas_x: number, canvas_y: number, x: number = 0, y: number = 0) {
+		let colors = ["#111111","#333333","#7777777","#999999","#bbbbbb","#dddddd","#ffffff", "#f47b37", "#fcb338", "#fcc816"];
 		var rndColor= Math.floor((Math.random() * colors.length) + 1);
-		this.vx = Math.floor((Math.random() * 4) + 1) * (Math.floor(Math.random() * 2) ? 1 : -1);
-		this.vy = Math.floor((Math.random() * 4) + 1) * (Math.floor(Math.random() * 2) ? 1 : -1);;
+		this.vx = Math.floor((Math.random() * 15) + 1) * (Math.floor(Math.random() * 2) ? 1 : -1);
+		this.vy = Math.floor((Math.random() * 15) + 1) * (Math.floor(Math.random() * 2) ? 1 : -1);;
 		this.color = colors[rndColor];
 		this.radius = Math.floor((Math.random() * 5) + 1);
-		this.x =  Math.floor((Math.random() * canvas_x - 40) + 20);
-		this.y =  Math.floor((Math.random() * canvas_y - 40) + 20);
+		this.x = x;
+		this.y = y;
+		if (this.x == 0)	this.x =  Math.floor((Math.random() * canvas_x - 40) + 20);
+		if (this.y == 0)	this.y =  Math.floor((Math.random() * canvas_y - 40) + 20);
 	}
 
 	update(canvas_x: number, canvas_y: number) {
@@ -32,6 +35,8 @@ class Ball {
 			this.vy = -this.vy;
 		if (this.x <= 10 || this.x >= canvas_x)
 			this.vx = -this.vx;
+		if (this.removing && this.alpha > 0)
+			this.alpha -= 1;
 	}
 
 }
@@ -57,6 +62,7 @@ export class LoginpageComponent implements AfterViewInit {
 		this.ctx.beginPath();
 		this.ctx.arc(ball.x, ball.y, ball.radius, Math.PI * 2, false);
 		this.ctx.fillStyle = ball.color;
+		this.ctx.globalAlpha = ball.alpha / 500;
 		this.ctx.fill();
 		this.ctx.closePath();
 		ball.update(this.canvas.width, this.canvas.height);
@@ -73,19 +79,33 @@ export class LoginpageComponent implements AfterViewInit {
 	
 	update() {
 		this.clearCanvas();
+		this.ball = this.ball.filter(item => (item.alpha !== 0));
 		for (var ball of this.ball)
 			this.drawBall(ball);
+	}
+
+	clickEvent(event: any) {
+		const rect = this.canvas.getBoundingClientRect();
+		const x = event.clientX - rect.left;
+		const y = event.clientY - rect.top;
+		for (var ball of this.ball)
+			ball.removing = true;
+		for (let i = 1; i < 50; i++)
+			this.ball.push(new Ball(this.canvas.width, this.canvas.height, x, y));
 	}
 
 	ngAfterViewInit(): void {
 		this.canvas = this.pageCanvas.nativeElement;
 		this.resizeCanvas();
 		window.addEventListener('resize', () => { this.resizeCanvas() }, false);
+		window.addEventListener('mousedown', (e: any) => {
+			this.clickEvent(e);
+		  })
 		this.ctx = this.canvas.getContext("2d");
 		this.clearCanvas();
 		for (let i = 1; i < 30; i++)
 			this.ball.push(new Ball(this.canvas.width, this.canvas.height));
-		setInterval(() => { this.update() }, 1000/60);
+		setInterval(() => { this.update() }, 1800/60);
 	}
 
 	login()	{
