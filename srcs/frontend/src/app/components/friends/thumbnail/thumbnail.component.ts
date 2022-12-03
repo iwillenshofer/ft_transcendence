a2eb101c45ad8728852088c614d67d6fa5ccf85d
iwillens <igor@42sp.org.br>
2022-12-03 20:48:44 -0300

    thumbnail dropdown

--- srcs/frontend/src/app/components/friends/thumbnail/thumbnail.component.ts
+++ srcs/frontend/src/app/components/friends/thumbnail/thumbnail.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { UsersOnlineService } from 'src/app/services/users-online.service';
 import { FriendsService } from '../friends.service';
+import { faEllipsisVertical, faTableTennisPaddleBall, faTv, faIdCard, faLock, faUnlock, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/auth/auth.service';
+import { OnlineGameService } from '../../game/online-game.service';
 
 @Component({
   selector: 'app-thumbnail',
@@ -11,10 +14,21 @@ export class ThumbnailComponent {
 
 	constructor(
 		protected onlineService: UsersOnlineService,
-		protected friendService: FriendsService
+		protected friendService: FriendsService,
+		protected authService: AuthService,
+		protected gameService: OnlineGameService
 		) {}
 
+	protected icon: IconDefinition[] = [faEllipsisVertical, faTableTennisPaddleBall, faTv, faIdCard, faLock, faUnlock]
 	@Input() username: string = '';
 	@Input() avatar: string = '';
 	@Input() size: string = 'medium';
+	@Input() dropdown: boolean = true;
+	@Input() light: boolean = true;
+
+	ngOnInit(): void { }
+
+	challenge(player: any) {
+		this.gameService.challenge(player);
+	  }
 }
