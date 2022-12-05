f6d5420c8441248347f7fe897f234da9cbe17529
Roman <roman.bitetti@tutanota.com>
2022-12-04 22:49:34 -0500

    clean up dialog new room

--- srcs/frontend/src/app/components/dialogs/dialog-new-room/dialog-new-room.component.ts
+++ srcs/frontend/src/app/components/dialogs/dialog-new-room/dialog-new-room.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { BehaviorSubject } from 'rxjs';
-import { AlertsService } from 'src/app/alerts/alerts.service';
+import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
-import { User } from 'src/app/auth/user.model';
 import { RoomService } from 'src/app/services/room/room.service';
 import { isRoomNameTaken } from 'src/app/validators/async-room-name.validator';
 
@@ -13,7 +11,7 @@ import { isRoomNameTaken } from 'src/app/validators/async-room-name.validator';
   templateUrl: './dialog-new-room.component.html',
   styleUrls: ['./dialog-new-room.component.scss']
 })
-export class DialogNewRoomComponent implements OnInit {
+export class DialogNewRoomComponent implements OnInit, OnDestroy {
 
   hide = true;
   IsProtected: boolean = true;
@@ -25,6 +23,8 @@ export class DialogNewRoomComponent implements OnInit {
     password: new FormControl(null, [Validators.required, Validators.minLength(8)])
   });
 
+  subscription1$!: Subscription;
+
   constructor(private roomService: RoomService,
     private authService: AuthService,
     private dialogRef: MatDialogRef<DialogNewRoomComponent>) {
@@ -32,31 +32,41 @@ export class DialogNewRoomComponent implements OnInit {
 
   ngOnInit(): void {
     this.form.controls['password'].disable();
-    this.authService.getLogoutStatus.subscribe((data) => {
+
+    this.subscription1$ = this.authService.getLogoutStatus.subscribe((data) => {
       if (data === true) {
         this.dialogRef.close();
       }
     });
   }
 
-  get name(): FormControl {
-    return this.form.get('name') as FormControl;
+  ngOnDestroy(): void {
+    this.subscription1$.unsubscribe();
   }
 
   get description(): FormControl {
     return this.form.get('description') as FormControl;
   }
 
-  get type(): FormControl {
-    return this.form.get('type') as FormControl;
+  get name(): FormControl {
+    return this.form.get('name') as FormControl;
   }
 
   get password(): FormControl {
     return this.form.get('password') as FormControl;
   }
 
-  OnSelectChange(input: string) {
+  get type(): FormControl {
+    return this.form.get('type') as FormControl;
+  }
 
+  createChatroom() {
+    if (this.form.valid)
+      this.roomService.createRoom(this.form.getRawValue());
+    this.dialogRef.close({ data: this.form.getRawValue() });
+  }
+
+  OnSelectChange(input: string) {
     switch (input) {
       case "3": {
         this.form.controls['password'].enable();
@@ -69,12 +79,4 @@ export class DialogNewRoomComponent implements OnInit {
       }
     }
   }
-
-  createChatroom() {
-    if (this.form.valid) {
-      this.roomService.createRoom(this.form.getRawValue());
-    }
-    this.dialogRef.close({ data: this.form.getRawValue() });
-  }
 }
-
